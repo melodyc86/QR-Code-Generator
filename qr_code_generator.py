2886523ed694c0ce14b3961d3c4f30e85c9b8d54
Melody Chirinos <91234711+melodyc86@users.noreply.github.com>
2024-08-05 16:18:46 -0400

    Add files via upload

---
+++ qr_code_generator.py
@@ -0,0 +1,29 @@
+# to use this, please install the qrcode library
+# pip install qrcode[pil]
+
+import qrcode
+
+def generate_qr_code(link, filename):
+    # Create a QR code object with the provided link
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(link)
+    qr.make(fit=True)
+
+    # Create an image from the QR code object
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    # Save the image to the specified filename
+    img.save(filename)
+    print(f"QR code generated and saved as {filename}")
+
+if __name__ == "__main__":
+    # Replace the link and filename with your desired values
+    web_link = "http://melodychirinos.notion.site"
+    output_filename = "Portfolio_qrcode.png"
+
+    generate_qr_code(web_link, output_filename)
